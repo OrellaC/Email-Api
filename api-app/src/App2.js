ef88be03900833bad8a1c4623a695162cfdfb75a
orellachichester@gmail.com <orellachichester@gmail.com>
2022-01-17 21:31:01 -0500

    Email API Commit

--- api-app/src/App2.js
+++ api-app/src/App2.js
@@ -3,25 +3,58 @@ import React, { Component } from 'react';
 class App2 extends Component {
     state = {
         status: "",
+        userInput: '',
         data: []
     }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
 
-
-
-
-
-
-
-
-
-
+    handleSubmit = e => {
+        e.preventDefault()
+        axios.get(`https://api.eva.pingutil.com/email?email= ${this.state.userInput}`)
+            .then(res => this.setState({
+                data: res.data.collection.items
+            }))
+    }
     render() {
+        console.log(this.state.data);
         return (
             <div>
+                {/* Creating a form allows user to input text for the search query in the above link */}
 
-            </div>
-        );
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='userInput'>Insert Email Address: </label>
+                    <input
+                        type='text'
+                        id='userInput'
+                        name='userInput'
+                        onChange={this.handleChange}
+                        value={this.state.userInput}
+                    />
+                    <input type='submit' value='submit' />
+                </form>
+
+                <div>
+                    {
+                        this.state.data.map((desc) => {
+                            return (
+                                <div>
+                                    <p>Email Address: {desc.data[0].email_address}</p>
+                                    <p>Domain: {desc.data[0].domain}</p>
+                                    <p>Valid Syntax: {desc.data[0].valid_syntax}</p>
+                                    <p>Disposable: {desc.data[0].disposable}</p>
+                                    
+                                    </div>
+                            )
+                        })
+                    }
+                    </div>
+                </div>
+                );
     }
 }
 
-export default App2;
+                export default App2;
